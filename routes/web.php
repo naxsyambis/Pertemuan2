65368a17aa63a74d7cc163aa44b3e31d339f9f38
naxsyambis <naxsyambis@gmail.com>
2026-03-19 17:51:09 +0700

    feat: mendefinisikan route pada setiap CRUD

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware('auth')->group(function () {
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+    Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+    Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+    Route::put('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
+    Route::get('/product/edit/{product}', [ProductController::class, 'edit'])->name('product.edit');
+    Route::delete('/product/delete/{id}', [ProductController::class, 'delete'])->name('product.delete');
+});
+
 require __DIR__.'/auth.php';
